e54208f200925de328812af47422fbde3c232051
Davide Almeida <davide.jhonatan@gmail.com>
2024-09-07 23:52:06 -0300

    fix contacts fields

--- db/migrate/20240907143710_create_contacts.rb
+++ db/migrate/20240907143710_create_contacts.rb
@@ -4,10 +4,12 @@ class CreateContacts < ActiveRecord::Migration[7.2]
   def change
     create_table :contacts do |t|
       t.string :name
-      t.string :cpf_code, unique: true
+      t.string :cpf_code
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :contacts, :cpf_code, unique: true
   end
 end
