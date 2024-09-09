db28339096d25913f3bc6740fd926e9a6ab2cf24
Davide Almeida <davide.jhonatan@gmail.com>
2024-09-08 23:07:41 -0300

    refact users_controller when when there arent users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::UsersController < ApplicationController
   def index
     users = User.all
 
+    return render json: {
+      message: "No users found"
+    }, status: :not_found if users.empty?
+
     render json: users
   end
 
