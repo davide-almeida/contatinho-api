c517d578563ae313a7eafe8a857cc7f2b5cb810d
Davide Almeida <davide.jhonatan@gmail.com>
2024-09-08 22:49:23 -0300

    fix devise custom mail template

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :contacts, allow_destroy: true
 
-  private
-
   # Sobrescrevendo o método send_reset_password_instructions do Devise
   # para exibir o template (app/views/user_mailer/reset_password_instructions.html.erb) do email corretamente.
   def send_reset_password_instructions
